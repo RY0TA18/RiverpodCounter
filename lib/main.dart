add2d5cf63bb683b1f0b98fbefc4805f8d6c1549
mizushima <mizushima@mizushimanoMacBook-Pro.local>
2024-09-08 23:00:22 +0900

    riverpodのコード生成を使用したカウンタ

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,61 @@
-part 'main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MaterialApp(
+        home: HomePage(title: 'Flutter Riverpod Example'),
+      ),
+    ),
+  );
+}
 
 @riverpod
-String greet(GreetRef ref) {
-  return 'Hello, World!';
+class CounterNotifier extends _$CounterNotifier{
+  @override
+  int build() => 0;
+
+  void increment() {
+    state++;
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterNotifierProvider);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have push the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(counterNotifierProvider.notifier).increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
